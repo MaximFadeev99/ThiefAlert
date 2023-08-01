b64095959f1475eb506066dd9c300b99e965b27c
MaximFadeev99 <fadeevmaksim701@gmail.com>
2023-08-01 22:28:25 +0300

    Accumulate scripts in one folder

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        bool isPressed = Input.anyKey;
+        float coveredDistanceWalk = 2;
+        float coveredDistanceIdle = 0;
+        float xPositionChange = coveredDistanceWalk * Time.deltaTime;
+
+        switch (isPressed) 
+        {
+            case true:
+
+                if (Input.GetKey(KeyCode.D))
+                {
+                    _animator.SetFloat("Speed", coveredDistanceWalk);
+                    transform.Translate(xPositionChange, 0, 0);
+                    break;
+                }
+                else if (Input.GetKey(KeyCode.A))
+                { 
+                    _animator.SetFloat("Speed", coveredDistanceWalk);
+                    transform.Translate(-xPositionChange, 0, 0);
+                    break;
+                }
+
+                break;
+
+            case false:
+                _animator.SetFloat("Speed", coveredDistanceIdle);
+                break;
+        }
+    }
+}
